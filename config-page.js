c095b89e5b35d48c8fd7712cf7de571fb3313538
Ananya Lucksorn <ananyalucksorn11@gmail.com>
2025-04-07 23:06:05 +0700

    Updated responsive design and animations

--- config-page.js
+++ config-page.js
@@ -1,6 +1,7 @@
 import { DRONE_ID } from "./config.js";
 
-const apiUrl = `http://localhost:3000/configs/${DRONE_ID}`;
+const apiUrl = `http://172.20.10.4:3000/...`;
+
 console.log("Fetching from:", apiUrl);  // ลอง log ดู
 
 async function loadConfig() {
