4eb2521e068bcd509b3a7963bbe04bb91ed308e6
Ananya Lucksorn <ananyalucksorn11@gmail.com>
2025-04-08 21:31:24 +0700

    Update///////

--- config-page.js
+++ config-page.js
@@ -1,22 +1,22 @@
 import { DRONE_ID } from "./config.js";
 
-const apiUrl = `https://abc1234.ap.ngrok.io/configs/${DRONE_ID}`;
+const apiUrl = `http://172.20.10.4:3000/configs/${DRONE_ID}`;
 
 
-console.log("Fetching from:", apiUrl);  // ลอง log ดู
+console.log("Fetching from:", apiUrl);  
 
 async function loadConfig() {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log("Data received:", data);  // ลอง log ดู
+    console.log("Data received:", data);  
 
     document.getElementById("drone-id").textContent = data.drone_id;
     document.getElementById("drone-name").textContent = data.drone_name;
     document.getElementById("light").textContent = data.light;
     document.getElementById("country").textContent = data.country;
   } catch (err) {
-    console.error("Error loading config:", err);  // ถ้ามี error
+    console.error("Error loading config:", err);  
   }
 }
 loadConfig();
--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 export const DRONE_ID = "64050738";
-export const API_BASE_URL = "https://abc1234.ap.ngrok.io"; // เปลี่ยนให้ตรงกับ ngrok
+export const API_BASE_URL = "http://172.20.10.4:3000/"; // เปลี่ยนให้ตรงกับ ngrok
